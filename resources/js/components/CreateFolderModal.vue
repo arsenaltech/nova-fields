f6091e3ec5e0234fd35e5aeffa828d6e600d4406
Parth KaPatel <parth.kapatel@arsenaltech.com>
2023-04-26 10:58:39 +0530

    fixed file manager move issue in FileManager Tool

--- resources/js/components/CreateFolderModal.vue
+++ resources/js/components/CreateFolderModal.vue
@@ -86,6 +86,7 @@ export default {
         } else {
           this.error = true;
           if (result.error) {
+            this.$emit('closeCreateFolderModal', true);
             this.errorPrompt = true;
             this.errorMsg = result.error;
             this.$toasted.show(this.__('Error:') + ' ' + result.error, {
