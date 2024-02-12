bda1e11c6aa82044beda2af544236ed14d42faf6
Aakashjoshi98 <aakash.joshi@arsenaltech.com>
2024-02-12 12:18:23 +0530

    Fixed - Space between rename button, chnages in error messege and added tooltip on copy, rename and delete button

--- resources/js/components/DetailPopup.vue
+++ resources/js/components/DetailPopup.vue
@@ -96,7 +96,7 @@
 
                       <svg @click="rename" xmlns="http://www.w3.org/2000/svg" class="ml-1 cursor-pointer text-success fill-current" viewBox="0 0 20 20" width="12" height="12"><path d="M0 11l2-2 5 5L18 3l2 2L7 18z"/></svg>
 
-                      <svg @click="editingName = !editingName" xmlns="http://www.w3.org/2000/svg" class="ml-1 cursor-pointer" viewBox="0 0 20 20" width="12" height="12"><path d="M2.93 17.07A10 10 0 1 1 17.07 2.93 10 10 0 0 1 2.93 17.07zm1.41-1.41A8 8 0 1 0 15.66 4.34 8 8 0 0 0 4.34 15.66zm9.9-8.49L11.41 10l2.83 2.83-1.41 1.41L10 11.41l-2.83 2.83-1.41-1.41L8.59 10 5.76 7.17l1.41-1.41L10 8.59l2.83-2.83 1.41 1.41z"/></svg>
+                      <svg @click="editingName = !editingName" xmlns="http://www.w3.org/2000/svg" class="ml-3 cursor-pointer" viewBox="0 0 20 20" width="12" height="12"><path d="M2.93 17.07A10 10 0 1 1 17.07 2.93 10 10 0 0 1 2.93 17.07zm1.41-1.41A8 8 0 1 0 15.66 4.34 8 8 0 0 0 4.34 15.66zm9.9-8.49L11.41 10l2.83 2.83-1.41 1.41L10 11.41l-2.83 2.83-1.41-1.41L8.59 10 5.76 7.17l1.41-1.41L10 8.59l2.83-2.83 1.41 1.41z"/></svg>
 
                     </template>
                   </template>
@@ -311,7 +311,7 @@ export default {
           this.$emit('refresh');
         } else {
           Nova.error(
-              this.__('Error renaming the file. Please check permissions'),
+              this.__('Error renaming the file'),
               { type: 'error' }
           );
         }
