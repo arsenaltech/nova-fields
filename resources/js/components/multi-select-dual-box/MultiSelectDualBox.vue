f4ba4811f15c948b99596b0f4f9b8ff5fdfb5253
keyursolanki <keyur.solanki@arsenaltech.com>
2024-07-26 17:41:10 +0530

    Fixed issue for not able to move option from right to left in create case

--- resources/js/components/multi-select-dual-box/MultiSelectDualBox.vue
+++ resources/js/components/multi-select-dual-box/MultiSelectDualBox.vue
@@ -170,10 +170,7 @@ export default {
     moveLeft(item) {
       let vue = this;
       if (typeof item == 'object') {
-        let index = vue.options.options.indexOf(item) > -1 ? vue.options.options.indexOf(item) : null;
-        if(this.resourceId != undefined && this.resourceId.length > 0){
-          index = vue.options.selected.indexOf(item) > -1 ? vue.options.selected.indexOf(item) : null;
-        }
+        let index = vue.options.selected.indexOf(item) > -1 ? vue.options.selected.indexOf(item) : null;
         if(index != null){
           vue.options.options.push(vue.options.selected[index]);
           let idIndex = vue.options.selectedIds.indexOf(vue.options.selected[index].value);
