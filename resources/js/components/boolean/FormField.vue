99f69ab6ca3d9dff5da95e9ece26e82bb6c2f0ce
Mahesh Thakor <mahesh.thakor@arsenaltech.com>
2025-07-28 14:42:09 +0530

    [EC-35721] Improve Boolean Handling in Checkbox FormField Component

--- resources/js/components/boolean/FormField.vue
+++ resources/js/components/boolean/FormField.vue
@@ -35,7 +35,14 @@ export default {
 
   computed: {
     checked() {
-      return Boolean(this.value)
+      const value = this.value;
+      if (value === true || value === 'true' || value === 1 || value === '1') {
+        return true;
+      }
+      if (value === false || value === 'false' || value === 0 || value === '0') {
+        return false;
+      }
+      return Boolean(value);
     },
 
     trueValue() {
